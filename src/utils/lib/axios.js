5401cc330f1e1a92167b9e9461df0c37ed0c3a52
Hemal99 <h.githmin@gmail.com>
2023-06-25 19:17:41 +0530

    update

--- src/utils/lib/axios.js
+++ src/utils/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// const baseUrl = "https://prompt-rating-e4d47952e7d8.herokuapp.com/";
+const baseUrl = "https://prompt-rating-e4d47952e7d8.herokuapp.com/";
 
-const baseUrl = "http://localhost:5000";
+// const baseUrl = "http://localhost:5000";
 
 const axiosInstance = axios.create({
   baseURL: baseUrl,
